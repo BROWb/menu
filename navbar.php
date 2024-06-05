48bb2c2c68885f019ec5c146a6cfc953699c46b1
BROWb <127416062+BROWb@users.noreply.github.com>
2024-06-05 13:31:42 -0400

    .

---
+++ navbar.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/navbar.css">
+    
+</head>
+<body>
+    
+<header>
+
+    <a href="#" class="logo">LOGO</a>
+
+    <input type="checkbox" id="menu-bar">
+    <label for="menu-bar">MENU</label>
+
+
+<nav class="navbar">
+    <ul>
+        <li><a href="#">home</a></li>
+        <li><a href="#">componentes</a></li>
+        <li><a href="#">quem somos</a></li>
+        <li><a href="#">manutençao +</a>
+            <ul>
+                <li><a href="#">dfhgf</a></li>
+                <li><a href="#">dfhgdfh</a></li>
+                <li><a href="#">vhvd</a></li>
+                <li><a href="#">hdgher</a></li>
+                <li><a href="#">fghdfy</a></li>
+                <li><a href="#">eryey</a></li>
+                <li><a href="#">eryey</a></li>
+                <li><a href="#">eryey</a></li>
+            
+            </ul>
+        </li>
+    </ul>
+</nav>
+
+</header>
+
+</body>
+</html>
